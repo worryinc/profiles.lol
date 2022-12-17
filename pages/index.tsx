5c8763e84701947b4720dfca1383ab46f5ab02b8
Hazel Thomas <mail@kannadev.com>
2022-12-17 03:30:57 +0000

    Scott forced me to commit
    Co-authored-by: Koji <kowoji@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,10 +15,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+        <h2 className={inter.className}>
+              Welcome to profiles.lol
+            </h2>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -28,11 +26,11 @@ export default function Home() {
             >
               By{' '}
               <Image
-                src="/vercel.svg"
+                src="/image2.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
-                height={24}
+                height={100}
                 priority
               />
             </a>
